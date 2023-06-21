b8de285afda6b62a5209bc151d1a5f9caf1b1f8d
Garguth <just7166@gmail.com>
2023-06-21 14:58:18 -0400

    Adding require.js to make the api work

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,11 @@
 const mtg = require('mtgsdk')
 
-function mtgCardFind() {
-
-    mtg.card.find(3)
-        .then(result => {
-            console.log(result.card.name) // "Black Lotus"
-        })
-}
+mtg.card.find(3)
+    .then(result => {
+        console.log(result.card.name) // "Black Lotus"
+    })
+    
+mtg.set.find('AER')
+    .then(result => {
+        console.log(result.set.name) // "Aether Revolt"
+    })
